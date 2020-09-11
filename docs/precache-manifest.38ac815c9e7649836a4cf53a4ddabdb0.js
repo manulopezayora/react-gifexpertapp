f1b75f063bd407449fe3c2d1509479a4486e43e5
ManuelJesusLopez <manulopezayora1985@protonmail.com>
2020-09-11 17:12:10 +0200

    docs added

---
+++ docs/precache-manifest.38ac815c9e7649836a4cf53a4ddabdb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa597e541d43f2a7a0bcd8d17cd38f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73d770789efa9abc91a4",
+    "url": "/static/css/main.c2e4e406.chunk.css"
+  },
+  {
+    "revision": "ddfc370ede98d9e59777",
+    "url": "/static/js/2.75f7596a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75f7596a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d770789efa9abc91a4",
+    "url": "/static/js/main.cab57d12.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
